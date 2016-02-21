1219ac7fc5d318edbaeba27294b1877147ea3d17
Christian Lepson <christian.lepson@gmail.com>
2016-02-20 21:01:14 -0500

    An Interactive, Searchable Image Gallery
    Project 4 of Treehouse’s web development techdegree.

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var $search = $('#search');
+var $gallery = $('.gallery');
+
+function showResults(value) {
+	// If search value is not empty
+	if(value.length != 0) {
+		//Hide all images initially
+		$gallery.hide();
+		$gallery.removeAttr('data-lightbox');
+		//If image title attribute contains search value, show image
+		$('.gallery[data-title*=' + value + ']').show();
+		$('.gallery[data-title*=' + value + ']').attr('data-lightbox', 'gallery');
+	} else {
+		// Show all images when search value is empty
+		$gallery.show();
+		$gallery.attr('data-lightbox', 'gallery');
+	}
+}
+
+//When user changes search value
+$search.keyup( function() {
+	//Pass search value to showResults function
+	var searchVal = $(this).val();
+	showResults(searchVal);
+});
